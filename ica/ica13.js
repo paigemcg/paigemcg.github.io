70b649b87bffd48bff15080e23b6a47ded2b4c2a
paigemcg <pamc7636@colorado.edu>
2024-04-16 18:18:13 -0600

    balls??

--- ica/ica13.js
+++ ica/ica13.js
@@ -97,7 +97,7 @@ class Ball {
 
 const balls = [];
 
-while (balls.length < 10){
+while (balls.length < 25){
     const size = random(1,20);
     const ball = new Ball (
         random(0 + size, width - size),
